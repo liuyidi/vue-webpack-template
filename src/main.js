229dd6a11a0fab4bbc8ee0c054f8d94b136ac535
liuyidi <928171466@qq.com>
2018-04-18 00:47:10 +0800

    init src content

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+import store from './store'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+})
